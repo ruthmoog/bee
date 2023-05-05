eda9e835105ebe4261ce66f754c9f656aeeb2d78
Ruth Baker <ruthmoog@gmail.com>
2023-05-05 09:45:25 +0100

    basic handler

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type beeHttpHandler struct {
+}
+
+func (b beeHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
 
 func main() {
 	fmt.Println("Hello World!")
+	handler := beeHttpHandler{}
+	http.ListenAndServe(":8080", handler)
 }
