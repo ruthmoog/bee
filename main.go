666560297282c305ba89cf9f182825d676e676d9
Ruth Baker <ruthmoog@gmail.com>
2023-05-05 10:10:11 +0100

    get port from env if available

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello World!")
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, nil)
 }
