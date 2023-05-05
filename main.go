7077b702ae7683d602652266514e35aae2cf8c8d
Ruth Baker <ruthmoog@gmail.com>
2023-05-05 10:02:38 +0100

    static web server hello world

--- main.go
+++ main.go
@@ -5,15 +5,9 @@ import (
 	"net/http"
 )
 
-type beeHttpHandler struct {
-}
-
-func (b beeHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
-}
-
 func main() {
 	fmt.Println("Hello World!")
-	handler := beeHttpHandler{}
-	http.ListenAndServe(":8080", handler)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	http.ListenAndServe(":8080", nil)
 }
