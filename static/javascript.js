a6c6a77a432081c1c8cfb6605196021d16e7767a
Ruth Baker <ruthmoog@gmail.com>
2023-05-06 14:34:45 +0100

    Change data structure to support counting different castes

--- static/javascript.js
+++ static/javascript.js
@@ -1,5 +1,5 @@
 const sightingsStorageKey = "sightings";
-const button = document.getElementById("ClickBeeButton");
+const queenSpottedButton = document.getElementById("QueenSpotted");
 const speciesSelection = document.getElementById("species");
 
 function getSightings() {
@@ -17,26 +17,30 @@ function renderCount() {
     const result = sightings.reduce((speciesCount, sighting) => {
         const currentSpeciesCount = speciesCount[sighting.species];
         if (!currentSpeciesCount) {
-            speciesCount[sighting.species] = 1;
+            speciesCount[sighting.species] = {queen: 1};
         } else {
-            speciesCount[sighting.species]++;
+            speciesCount[sighting.species].queen++;
         }
         return speciesCount
     }, {});
+
+    console.log(result)
 
     const observations = document.getElementById("observations")
 
     observations.innerHTML = "";
 
-    for (const [species, count] of Object.entries(result)) {
+    for (const [species, casteCounts] of Object.entries(result)) {
 
         const row = observations.insertRow(0);
 
         const speciesCell = row.insertCell(0);
-        const countCell = row.insertCell(1);
+        const queensCell = row.insertCell(1);
+
+        console.log(casteCounts, "%%%%%%%%%%%%%%%%%%");
 
         speciesCell.innerHTML = species
-        countCell.innerHTML = count
+        queensCell.innerHTML = casteCounts.queen;
     }
 
 
@@ -44,11 +48,11 @@ function renderCount() {
 
 renderCount();
 
-button.addEventListener("click", () => {
+queenSpottedButton.addEventListener("click", () => {
     console.log("BUZZZZZZZ")
 
     let sightings = getSightings();
-    sightings.push({species: speciesSelection.value})
+    sightings.push({species: speciesSelection.value, caste: "queen"})
     localStorage.setItem(sightingsStorageKey, JSON.stringify(sightings));
     renderCount();
 })
