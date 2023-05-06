94e58f893a22567021b30a905e2d157db94a80ad
Ruth Baker <ruthmoog@gmail.com>
2023-05-06 13:44:14 +0100

    Extract js into js file

---
+++ static/javascript.js
@@ -0,0 +1,39 @@
+const sightingsStorageKey = "sightings";
+const button = document.getElementById("ClickBeeButton");
+const displayCount = document.getElementById("beeCount");
+const speciesSelection = document.getElementById("species");
+
+function getSightings() {
+    let sightings = localStorage.getItem(sightingsStorageKey);
+    if (!sightings) {
+        return [];
+    } else {
+        return JSON.parse(sightings);
+    }
+}
+
+function updateCount() {
+    let sightings = getSightings();
+    const result = sightings.reduce((speciesCount, sighting) => {
+        const currentSpeciesCount = speciesCount[sighting.species];
+        if (!currentSpeciesCount) {
+            speciesCount[sighting.species] = 1;
+        } else {
+            speciesCount[sighting.species]++;
+        }
+        return speciesCount
+    }, {});
+    console.log(result)
+    displayCount.innerText = JSON.stringify(result);
+}
+
+updateCount();
+
+button.addEventListener("click", () => {
+    console.log("BUZZZZZZZ")
+
+    let sightings = getSightings();
+    sightings.push({species: speciesSelection.value})
+    localStorage.setItem(sightingsStorageKey, JSON.stringify(sightings));
+    updateCount();
+})
