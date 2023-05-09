f3d426436b4424d1cd0502b82a37dadc98922a2f
Ruth Baker <ruthmoog@gmail.com>
2023-05-09 15:16:47 +0100

    Refactor bee summary

--- static/bundle.js
+++ static/bundle.js
@@ -1,8 +1,8 @@
 (function () {
     'use strict';
 
-    function createBeeSightingSummary (sightings) {
-        const beeSightingsSummary = sightings.reduce((summary, sighting) => {
+    function createBeeSightingSummary(sightings) {
+        return sightings.reduce((summary, sighting) => {
             // we need to update the section for the given sighting
             // look up the section from the summary (or create a new section)
 
@@ -10,24 +10,21 @@
             summary[sighting.section] = currentSection;
 
             // look up the species count in the current section
-            const currentSpeciesCount = currentSection[sighting.species];
+            let currentSpeciesCount = currentSection[sighting.species];
 
+            // if we haven't seen this caste (but we have seen the species)
             if (!currentSpeciesCount) {
                 // if the species hasnt been seen in this section, add a record of it, with its caste
                 currentSection[sighting.species] = {[sighting.caste]: 1};
+            } else if (!currentSection[sighting.species][sighting.caste]) {
+                // add the caste
+                currentSection[sighting.species][sighting.caste] = 1;
             } else {
-                // if we haven't seen this caste (but we have seen the species)
-                if (!currentSection[sighting.species][sighting.caste]) {
-                    // add the caste
-                    currentSection[sighting.species][sighting.caste] = 1;
-                } else {
-                    // increment the caste
-                    currentSection[sighting.species][sighting.caste]++;
-                }
+                // increment the caste
+                currentSection[sighting.species][sighting.caste]++;
             }
             return summary
         }, {});
-        return beeSightingsSummary;
     }
 
     const sightingsStorageKey = "sightings";
