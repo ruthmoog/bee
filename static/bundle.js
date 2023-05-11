dd38c904db3e7fcaf6db8dd2039da2fc291c106c
Ruth Baker <ruthmoog@gmail.com>
2023-05-11 17:45:29 +0100

    use strong equals for clear event confirmation

--- static/bundle.js
+++ static/bundle.js
@@ -124,7 +124,7 @@
     clearButton.addEventListener("click", () => {
         const warningClearStoredData = "Make sure you have saved or submitted your data before proceeding.\n\n" +
             "Delete forever?";
-        if (confirm(warningClearStoredData) == true) {
+        if (confirm(warningClearStoredData) === true) {
             localStorage.clear();
             location.reload();
         }
