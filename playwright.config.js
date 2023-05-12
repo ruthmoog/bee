0b6f4b8e7610853e8b9b9c3afa6a1b9b2e3ec0cd
Chris James <quii@hey.com>
2023-05-12 20:11:20 +0100

    playwright will launch the server if it's not already running

--- playwright.config.js
+++ playwright.config.js
@@ -77,10 +77,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the browser_tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:8080',
+    reuseExistingServer: true,
+  },
 });
 
