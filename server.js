d7226487fecd66b8873b7b1e1eb6f00e35094932
Chris James <quii@hey.com>
2023-05-12 19:41:51 +0100

    fixed module problem for server, we may be able to put playwright at root now!

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var StaticServer = require('static-server');
+import StaticServer from 'static-server';
 var server = new StaticServer({
     rootPath: './static',            // required, the root of the server file tree
     port: 5001,               // required, the port to listen
